320e1e7e598add792631ce5b7f965b5418400cc8
maks-nurgazy <maksatbek.bolushov@gmail.com>
2021-02-22 17:36:06 +0600

    create superuser fault

--- users/managers.py
+++ users/managers.py
@@ -17,25 +17,16 @@ class UserManager(BaseUserManager):
         return user
 
 
-class SuperAdminManager(UserManager):
+class AdminManager(UserManager):
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('role', "SUPER_ADMIN")
+        extra_fields.setdefault('role', "ADMIN")
 
         if extra_fields.get('role') != 'ADMIN':
             raise ValueError('Superuser must have role of Global Admin')
-        return self.create_user(email, password, **extra_fields)
-
-
-class AdminManager(UserManager):
-
-    def create_admin(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_active', True)
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('role', "ADMIN")
         return self.create_user(email, password, **extra_fields)
 
     def get_queryset(self, *args, **kwargs):
