5716a2cbc2a6fd6d426a1210cfd12f46094d8e45
maks-nurgazy <maksatbek.bolushov@gmail.com>
2021-02-22 16:33:19 +0600

    try remove .idea

--- library_management/settings.py
+++ library_management/settings.py
@@ -22,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e9_3zcn1+mm_&=%&l)$-m#67ps9-r3gk)11fy(imkiu(bg#k8*'
+# SECRET_KEY = 'e9_3zcn1+mm_&=%&l)$-m#67ps9-r3gk)11fy(imkiu(bg#k8*'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'e9_3zcn1+mm_&=%&l)$-m#67ps9-r3gk)11fy(imkiu(bg#k8*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
