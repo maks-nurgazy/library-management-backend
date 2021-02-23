99c676f58399a0d84d96e92b8a02067c18082598
maks-nurgazy <maksatbek.bolushov@gmail.com>
2021-02-23 17:19:01 +0600

    Forget change back to Adminmanager

--- users/models.py
+++ users/models.py
@@ -39,7 +39,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
 
-    objects = UserManager()
+    objects = AdminManager()
 
     def __str__(self):
         return self.email
